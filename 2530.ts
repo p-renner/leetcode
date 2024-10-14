c8f78e6edac60e3c40b47d46927067a8ff31812c
Philipp Renner <phillianus@gmail.com>
2024-10-14 09:36:10 +0200

    daily: 2024-10-14

---
+++ 2530.ts
@@ -0,0 +1,23 @@
+import { MaxPriorityQueue } from '@datastructures-js/priority-queue';
+
+function maxKelements(nums: number[], k: number): number {
+	const pq = new MaxPriorityQueue<number>();
+	let score = 0;
+
+	for (const num of nums) {
+		pq.enqueue(num);
+	}
+
+	for (let i = 0; i < k; i++) {
+		const max = pq.dequeue();
+		score += max.element;
+		pq.enqueue(Math.ceil(max.element / 3));
+	}
+
+	return score;
+}
+
+test('maxKelements', () => {
+	expect(maxKelements([10, 10, 10, 10, 10], 5)).toBe(50);
+	expect(maxKelements([1, 10, 3, 3, 3], 3)).toBe(17);
+});
