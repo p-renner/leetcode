d5094e8e60b1cf6439176b3759aabe73f54e163f
Philipp Renner <phillianus@gmail.com>
2024-12-12 08:29:54 +0100

    daily: 2024-12-12

---
+++ 2558.ts
@@ -0,0 +1,21 @@
+import { MaxPriorityQueue } from '@datastructures-js/priority-queue';
+
+function pickGifts(gifts: number[], k: number): number {
+	const maxHeap = new MaxPriorityQueue<number>();
+	for (let gift of gifts) {
+		maxHeap.enqueue(gift, gift);
+	}
+
+	for (let i = 0; i < k; i++) {
+		const maxGifts = maxHeap.dequeue().element;
+		const root = Math.floor(Math.sqrt(maxGifts));
+		maxHeap.enqueue(root);
+	}
+
+	return maxHeap.toArray().reduce((acc, curr) => acc + curr.element, 0);
+}
+
+test('pickGifts', () => {
+	expect(pickGifts([25, 64, 9, 4, 100], 4)).toBe(29);
+	expect(pickGifts([1, 1, 1, 1], 4)).toBe(4);
+});
