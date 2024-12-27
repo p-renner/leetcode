0b27569294f2f03b7f5aacefce8ee490ba43b4c6
Philipp Renner <phillianus@gmail.com>
2024-12-27 15:45:50 +0000

    daily: 2024-12-27

--- 1014.ts
+++ 1014.ts
@@ -5,6 +5,21 @@ function maxScoreSightseeingPair(values: number[]): number {
 	)[0];
 }
 
+//function maxScoreSightseeingPair(values: number[]): number {
+//	let res = 0;
+//	let i = 0;
+//
+//	for (let j = 1; j < values.length; j++) {
+//		res = Math.max(values[i] + values[j] + i - j, res);
+//
+//		if (values[i] + i - j <= values[j]) {
+//			i = j;
+//		}
+//	}
+//
+//	return res;
+//}
+
 test('maxScoreSightseeingPair', () => {
 	expect(maxScoreSightseeingPair([8, 1, 5, 2, 6])).toBe(11);
 	expect(maxScoreSightseeingPair([1, 2])).toBe(2);
