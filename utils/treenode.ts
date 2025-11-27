36ccea0f353b89d7e6833eb101071bd24262898a
Philipp Renner <phillianus@gmail.com>
2025-11-27 14:16:40 +0100

    fix(treeFromArr): fix uneven bug

--- utils/treenode.ts
+++ utils/treenode.ts
@@ -10,23 +10,28 @@ export class TreeNode {
 }
 
 export function treeFromArr(arr: Array<number | null>): TreeNode | null {
-	if (arr.length === 0) return null;
+	if (arr.length === 0) {
+		return null;
+	}
+
 	const root = new TreeNode(arr[0]!);
 	const queue = [root];
 	let i = 1;
+
 	while (i < arr.length) {
 		const current = queue.shift()!;
-		const left = arr[i++];
-		if (left !== null) {
-			current.left = new TreeNode(left);
+
+		if (i < arr.length && arr[i] !== null) {
+			current.left = new TreeNode(arr[i]);
 			queue.push(current.left);
 		}
+		i++;
 
-		const right = arr[i++];
-		if (right !== null) {
-			current.right = new TreeNode(right);
+		if (i < arr.length && arr[i] !== null) {
+			current.right = new TreeNode(arr[i]);
 			queue.push(current.right);
 		}
+		i++;
 	}
 
 	return root;
