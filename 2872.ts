743d7a5704fc6c652db637eb29bcd4559f8e6187
Philipp Renner <phillianus@gmail.com>
2025-11-28 11:10:39 +0100

    daily: 2025-11-28

--- 2872.ts
+++ 2872.ts
@@ -1,33 +1,33 @@
 function maxKDivisibleComponents(n: number, edges: number[][], values: number[], k: number): number {
-	const adj = new Map<number, number[]>();
+	const connections = new Map<number, number[]>();
 
 	for (const [a, b] of edges) {
-		if (!adj.has(a)) {
-			adj.set(a, []);
+		if (!connections.has(a)) {
+			connections.set(a, []);
 		}
 
-		if (!adj.has(b)) {
-			adj.set(b, []);
+		if (!connections.has(b)) {
+			connections.set(b, [a]);
 		}
 
-		adj.get(a).push(b);
-		adj.get(b).push(a);
+		connections.get(a).push(b);
+		connections.get(b).push(a);
 	}
 
 	let res = 0;
 
-	function dfs(node: number, parent: number): number {
-		let sum = values[node];
+	function traverse(curr: number, parent: number): number {
+		let sum = values[curr];
 
-		for (const child of adj.get(node) || []) {
-			if (child === parent) {
-				continue;
-			}
-
-			sum += dfs(child, node);
+		if (connections.has(curr)) {
+			sum += connections
+				.get(curr)
+				.filter((c) => c != parent)
+				.map((c) => traverse(c, curr))
+				.reduce((acc, c) => acc + c, 0);
 		}
 
-		if (sum % k === 0) {
+		if (sum % k == 0) {
 			res++;
 			return 0;
 		}
@@ -35,38 +35,46 @@ function maxKDivisibleComponents(n: number, edges: number[][], values: number[],
 		return sum;
 	}
 
-	dfs(0, -1);
+	traverse(0, -1);
 	return res;
 }
 
-test('maxKDivisibleComponents', () => {
-	expect(
-		maxKDivisibleComponents(
-			5,
-			[
-				[0, 2],
-				[1, 2],
-				[1, 3],
-				[2, 4],
-			],
-			[1, 8, 1, 4, 4],
-			6,
-		),
-	).toBe(2);
-	expect(
-		maxKDivisibleComponents(
-			7,
-			[
-				[0, 1],
-				[0, 2],
-				[1, 3],
-				[1, 4],
-				[2, 5],
-				[2, 6],
-			],
-			[3, 0, 6, 1, 5, 2, 1],
-			3,
-		),
-	).toBe(3);
-	expect(maxKDivisibleComponents(1, [], [0], 1)).toBe(1);
+describe('maxKDivisibleComponents', () => {
+	test('case 1', () => {
+		expect(
+			maxKDivisibleComponents(
+				5,
+				[
+					[0, 2],
+					[1, 2],
+					[1, 3],
+					[2, 4],
+				],
+				[1, 8, 1, 4, 4],
+				6,
+			),
+		).toBe(2);
+	});
+
+	test('case 2', () => {
+		expect(
+			maxKDivisibleComponents(
+				7,
+				[
+					[0, 1],
+					[0, 2],
+					[1, 3],
+					[1, 4],
+					[2, 5],
+					[2, 6],
+				],
+				[3, 0, 6, 1, 5, 2, 1],
+				3,
+			),
+		).toBe(3);
+	});
+
+	test('edgecase empty edges', () => {
+		expect(maxKDivisibleComponents(1, [], [0], 1)).toBe(1);
+	});
 });
